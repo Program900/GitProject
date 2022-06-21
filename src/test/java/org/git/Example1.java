e7771378532e11899530e5b71408691c28a3cf4f
Program900 <gitahemanth@gmail.com>
2022-06-21 09:53:05 +0100

    Added post request

---
+++ src/test/java/org/git/Example1.java
@@ -0,0 +1,48 @@
+package org.git;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.testng.annotations.Test;
+
+public class Example1 {
+
+    @Test
+    private void postRequestTest(){
+        RestAssured.given().baseUri("https://restful-booker.herokuapp.com/").basePath("booking")
+                .body("{\n" +
+                        "    \"firstname\" : \"DD\",\n" +
+                        "    \"lastname\" : \"BroDSwn\",\n" +
+                        "    \"totalprice\" : 111,\n" +
+                        "    \"depositpaid\" : true,\n" +
+                        "    \"bookingdates\" : {\n" +
+                        "        \"checkin\" : \"2018-01-01\",\n" +
+                        "        \"checkout\" : \"2019-01-01\"\n" +
+                        "    },\n" +
+                        "    \"additionalneeds\" : \"Breakfast\"\n" +
+                        "}").contentType(ContentType.JSON).post().then().log().all();
+
+
+
+    }
+    @Test
+    private void extractPostRequest(){
+       String Resposne= RestAssured.given().baseUri("https://restful-booker.herokuapp.com/").basePath("booking")
+                .body("{\n" +
+                        "    \"firstname\" : \"DD\",\n" +
+                        "    \"lastname\" : \"BroDSwn\",\n" +
+                        "    \"totalprice\" : 111,\n" +
+                        "    \"depositpaid\" : true,\n" +
+                        "    \"bookingdates\" : {\n" +
+                        "        \"checkin\" : \"2018-01-01\",\n" +
+                        "        \"checkout\" : \"2019-01-01\"\n" +
+                        "    },\n" +
+                        "    \"additionalneeds\" : \"Breakfast\"\n" +
+                        "}").contentType(ContentType.JSON).post().then().extract().body().asPrettyString();
+
+System.out.println("resposne"+Resposne);
+
+    }
+
+
+
+}
---
+++ src/test/java/org/git/Example2.java
@@ -0,0 +1,23 @@
+package org.git;
+
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
+public class Example2 {
+
+    @Test
+    private void GetRequestForOneBooking(){
+        RestAssured.
+                given().baseUri("https://restful-booker.herokuapp.com").basePath("/booking/1").
+                when().get().
+                then().log().all().statusCode(200);
+    }
+
+    @Test
+    private void getRequestForAllBookings(){
+        RestAssured.
+                given().baseUri("https://restful-booker.herokuapp.com").basePath("/booking").
+                when().get().
+                then().log().all().statusCode(200);
+    }
+}
